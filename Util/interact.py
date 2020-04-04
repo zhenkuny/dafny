bb5bd4a266e719dd3eb4686cfdee803ffa2739b5
Bryan Parno <parno@cmu.edu>
2020-04-04 12:23:29 -0400

    Clean up and get Symbols request working

--- Util/interact.py
+++ Util/interact.py
@@ -15,6 +15,18 @@ import base64
 #from prompt_toolkit.key_binding import KeyBindings
 #from prompt_toolkit.validation import Validator
 
+class Task:
+    def __init__(self, args, file_label, sourceIsFile, file_name):
+        self.args = args
+        self.file_label = file_label
+        self.sourceIsFile = sourceIsFile
+        self.file_name = file_name
+
+    def to_dict(self):
+        return {"args" :         self.args, 
+                "filename" :     self.file_label,
+                "sourceIsFile" : self.sourceIsFile,
+                "source" :       self.file_name}
 
 class DafnyServer:
     def __init__(self, server_path):
@@ -33,48 +45,70 @@ class DafnyServer:
             exit()
 
 
+    def write(self, a_string):
+        self.pipe.stdin.write((a_string + '\n').encode(self.encoding))
+
     def send_version_query(self):
-        self.pipe.stdin.write('version\n'.encode(self.encoding))
-        self.pipe.stdin.write((self.CLIENT_EOM_TAG + '\n').encode(self.encoding))
+        self.write('version')
+        self.write(self.CLIENT_EOM_TAG)
         self.pipe.stdin.flush()
 
-    def send_verification_query(self): 
-        self.pipe.stdin.write('verify\n'.encode(self.encoding))
-        query = {"args" : [], "filename" : "Foo.cpp", "sourceIsFile" : True, "source" : "t.dfy"}
-        j = json.dumps(query)
-        print("JSON query: " + j)
-        st = json.dumps(query) 
-        b = st.encode(self.encoding)
-        b64 = base64.b64encode(b) 
-        #text = b64
-        #text = (base64.b64encode() + '\n')
-        #self.pipe.stdin.write(text.encode(self.encoding))
+    def write_verification_task(self, task): 
+        query = task.to_dict()
+        print(query)
+        j_string = json.dumps(query) 
+        b = j_string.encode(self.encoding) # Convert to bytes
+        b64 = base64.b64encode(b)          # Produce base64-encoded bytes
         self.pipe.stdin.write(b64)
-        self.pipe.stdin.write(('\n' + self.CLIENT_EOM_TAG + '\n').encode(self.encoding))
+        self.write('')                     # Add a newline
+
+    def send_symbol_query(self, task):
+        self.write('symbols')
+        self.write_verification_task(task)
+        self.write(self.CLIENT_EOM_TAG)
+        self.pipe.stdin.flush()
+
+    def send_verification_query(self, task):
+        self.write('verify')
+        self.write_verification_task(task)
+        self.write(self.CLIENT_EOM_TAG)
         self.pipe.stdin.flush()
 
     def recv_response(self):
-        response = None
-        #response = json.loads(self.pipe.stdout.readline().decode(self.encoding))
+        response = ""
         while True:
-            response = self.pipe.stdout.readline().decode(self.encoding)
+            line = self.pipe.stdout.readline().decode(self.encoding)
             
-            if response.startswith("[%s] %s" % (self.SUCCESS, self.SERVER_EOM_TAG)):
+            if line.startswith("[%s] %s" % (self.SUCCESS, self.SERVER_EOM_TAG)):
                 print("Ended in success")
                 break
-            elif response.startswith("[%s] %s" % (self.FAILURE, self.SERVER_EOM_TAG)):
+            elif line.startswith("[%s] %s" % (self.FAILURE, self.SERVER_EOM_TAG)):
                 print("Ended in failure")
                 break
             else:
-                print(response)
-#        while True:
-#            response = json.loads(self.pipe.stdout.readline().decode(self.encoding))
-#            if response['kind'] == 'response':
-#                if response['status'] == 'success':
-#                    break
-#                else:
-#                raise QueryError(response['response'])
+                response = response + line
+        print(response)
+        return response
 
+    def parse_symbols(self, response):
+        match = re.search("SYMBOLS_START (.*) SYMBOLS_END", response)
+        if match:
+            symbols = json.loads(match.group(1))
+            print(symbols)
+            return symbols
+        else:
+            print("Didn't find expected symbols in the server's response")
+            return []
+
+
+def find_functions_methods(symbols):
+    names = []
+    for sym in symbols:
+        if sym['SymbolType'] == 'Method' or sym['SymbolType'] == 'Function':
+            name = sym['Name']
+            if not name == '_default':
+                names.append(name)
+    return names
 
 def main():
     parser = argparse.ArgumentParser(description="Interact with the Dafny server")
@@ -82,10 +116,15 @@ def main():
     args = parser.parse_args()
 
     server = DafnyServer('./Binaries/dafny-server')
+    label = "Foo.cpp"
+    task = Task([], label, True, "t.dfy")
     #sys.stdin.readline()
     #server.send_version_query()
-    server.send_verification_query()
-    server.recv_response()
+    #server.send_verification_query(task)
+    server.send_symbol_query(task)
+    response = server.recv_response()
+    symbols = server.parse_symbols(response)
+    print(find_functions_methods(symbols))
 
     #event_loop()
 
